fc3a0730b20601d64c756a77aad9b4d3cec414fe
OpenVentura // nGenOne <147881592+OpenVentura@users.noreply.github.com>
2024-02-09 09:34:59 -0700

    Create russianRoulette.cpp

---
+++ bullshit/russianRoulette.cpp
@@ -0,0 +1,23 @@
+#include "bullshit.h"
+using namespace std;
+namespace fs = std::filesystem;
+
+int main() {
+	
+	int guess;
+	cout << "Lets play a game of russian roulette :) :3\n";
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<>dis(1, 8);
+	int pull = dis(gen);
+	cout << "enter a number between 1 and 6\n";
+	cin >> guess;
+	if (guess == pull) {
+		cout << "w";
+	}
+	else {
+		string delPath = "C://Users/OpenVentura/Desktop/test.txt";
+		fs::remove(delPath);
+	}
+
+}
